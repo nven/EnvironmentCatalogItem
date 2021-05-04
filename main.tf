abc1102036afe2338085b6df6db0978f3a2c0f6b
Venkatesh <nvenkatesh.27@gmail.com>
2021-05-04 19:55:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 
 # Create an arbitrary local resource
 data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var} and my sensitive value is = $${sensitive_var}"
+  template = "Hello, I am a template. My sample_var value"
 
   vars = {
-    sample_var = var.sample_var
-   sensitive_var = var.sensitive_var
+    sample_var = "test"
   }
 }
